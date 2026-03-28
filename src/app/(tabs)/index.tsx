aae0519b23930c7e9f3d7479729fae313d90c828
thayvitareli <thainyvitareli@hotmail.com>
2026-03-27 21:20:47 -0300

    refactor: home view props on home screen

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,8 +1,10 @@
 import HomeView from '@/features/home/home.view';
+import { useHomeViewModel } from '@/features/home/home.view-model';
 
 export default function HomeScreen() {
+  const { handleNavigateToDetails, coffeeShops, location, errorMsg, isLoading, requestPermissionAndFetchLocation } = useHomeViewModel();
 
   return (
-    <HomeView />
+    <HomeView handleNavigateToDetails={handleNavigateToDetails} coffeeShops={coffeeShops} location={location} errorMsg={errorMsg} isLoading={isLoading} requestPermissionAndFetchLocation={requestPermissionAndFetchLocation} />
   );
 }
