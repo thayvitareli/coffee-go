3603681bf27f9a89ca7c34af8438511185dffb3b
thayvitareli <thainyvitareli@hotmail.com>
2026-04-02 17:47:25 -0300

    feat: add changeLanguage on useProfileViewModel

--- src/features/profile/profile.view-model.tsx
+++ src/features/profile/profile.view-model.tsx
@@ -2,14 +2,19 @@ import { useAuthStore } from '@/store/use-auth-store';
 import { useVisitedStore } from '@/store/use-visited-store';
 import { useRouter } from 'expo-router';
 import { CoffeeShop } from '@/features/home/home.model';
-
+import { useTranslation } from 'react-i18next';
 export const useProfileViewModel = () => {
     const { user, signOut } = useAuthStore();
     const visitedShops = useVisitedStore(state => state.visitedShops);
     const router = useRouter();
+    const { i18n } = useTranslation();
     
     const handleNavigateToDetails = (coffeeShop: CoffeeShop) => {
         router.push(`/details/${coffeeShop.id}` as any);
+    };
+
+    const changeLanguage = (lang: string) => {
+        i18n.changeLanguage(lang);
     };
 
     const handleSignOut = () => {
@@ -21,6 +26,8 @@ export const useProfileViewModel = () => {
         user,
         signOut: handleSignOut,
         visitedShops,
-        handleNavigateToDetails
+        handleNavigateToDetails,
+        changeLanguage,
+        currentLanguage: i18n.language
     }
 }
